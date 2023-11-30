44730d5b0f9cd1f87c4f63e1963e029e7ed3eda4
alex-artamonov <alex.artamonov2010@gmail.com>
2023-12-01 00:23:31 +0100

    small cleanup-2

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,27 +87,6 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-# DATABASES = {
-#     "psql": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "blog",
-#         "USER": "blog",
-#         "PASSWORD": 'asdf',
-#         "PORT": 5432,
-#         "HOST": 'localhost'
-#     },
-#     "sqlite": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 from . import env
 DATABASES = env.DATABASES
 # default_database = environ.get('DJANGO_DATABASE', 'psql')
